d8e567e6e7bd2c19189218a4060cab62ffd71303
Nicolas Berard <nicolas.berard@canaltp.fr>
2017-06-29 17:37:22 +0200

    read ok from slack video

--- launch.py
+++ launch.py
@@ -13,11 +13,13 @@ full_url = '?'.join([slack_endpoint + uri, params])
 print(full_url)
 # https://slack.com/api/channels.history?token=xoxp-16812062839-41309578433-204906609120-73921600c18cbcaa82217e35004a59ff&channel=C60NEG4QG&pretty=1
 with urllib.request.urlopen(full_url) as response:
-   channel_messages = json.loads(response.read().decode())
-   print(channel_messages)
-   call(['vlc', '-f https://www.youtube.com/watch?v=cTSjyXL4_MQ'])
-   exit()
-   for channel_message in channel_messages:
-       if type == 'message' and len(channel_message['attachments']) and channel_message['attachments'][0] == 'YouTube':
-            call(['vlc', '-f ' + channel_message['text'][1:-1]])
+    channel_messages = json.loads(response.read().decode())
+    print(len(channel_messages))
+    # call(['vlc', '-f', 'https://www.youtube.com/watch?v=cTSjyXL4_MQ'])
+    for channel_message in channel_messages['messages']:
+        if channel_message['type'] == 'message' and 'attachments' in channel_message and len(
+                channel_message['attachments']) > 0 and channel_message['attachments'][0]['service_name'] == 'YouTube':
+            url_to_read = channel_message['text'][1:-1].replace('\\', '')
+            print(url_to_read)
+            call(['vlc', '-f', url_to_read])
 print('end')
