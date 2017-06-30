adbdbe98940f43e8477e49b41ced3f1bb50ebbd8
bbonnesoeur <bertrand.bonnesoeur@canaltp.fr>
2017-06-30 14:42:55 +0200

    first version of stop feature

---
+++ stop.py
@@ -0,0 +1,15 @@
+import subprocess, signal
+import time
+import os
+
+while True:
+	p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+	out, err = p.communicate()
+
+	for line in out.splitlines():
+		print(line)
+		if 'vlc' in line:
+			print('process vlc found ! waiting 6 seconds before kill !!!')
+			time.sleep(6)
+			pid = int(line.split(None, 1)[0])
+			os.kill(pid, signal.SIGKILL)
